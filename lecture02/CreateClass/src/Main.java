a358cb9d863a22299c3ab23cb59d5e7578c325e9
aoha <aoha.kurobane168@gmail.com>
2022-11-19 21:22:09 +0900

    Mainで持っていたメソッドをPokemonクラスに移動と、複数持つメンバは複数形に名称を変更

--- lecture02/CreateClass/src/Main.java
+++ lecture02/CreateClass/src/Main.java
@@ -2,32 +2,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void changeSkill(List<Pokemon> pokemonList, List<String> skill, int index) {
-        if(pokemonList.size() > index) {
-            pokemonList.get(index).setSkill(skill);
-        }
-    }
-
-    public static void changeMeow(List<Pokemon> pokemonList, String meow, int index) {
-        if(pokemonList.size() > index) {
-            pokemonList.get(index).setMeow(meow);
-        }
-    }
-
-    public static void printMessage(List<Pokemon> pokemonList) {
-        System.out.println("---------------------------------");
-        for(Pokemon pokemon : pokemonList) {
-            System.out.println("作成したポケモンは以下のようになりました");
-            System.out.println("名称：" + pokemon.getName());
-            System.out.println("タイプ：" + pokemon.getType());
-            System.out.println("技：" + pokemon.getSkill());
-            System.out.println("なきごえ：" + pokemon.getMeow());
-            System.out.println("---------------------------------");
-        }
-    }
-
     public static void main(String[] args) {
         System.out.println("ポケモンを作成します");
+        // ポケモンを作成
         List<Pokemon> pokemonList = List.of(new Pokemon("ヒトカゲ",
                                                             List.of("ほのおタイプ"),
                                                             Arrays.asList("ひっかく","ひのこ"),
@@ -41,15 +18,21 @@ public class Main {
                                                             Arrays.asList("そらをとぶ","しっぽをふる","つばさでうつ","かみつく"),
                                                             "バァァヴァァァッッ！")
                                             );
-        printMessage(pokemonList);
+        // 作成したポケモンを表示
+        for(Pokemon pokemon : pokemonList) {
+            System.out.println(pokemon.toString());
+        }
 
+        // リザードンの技と鳴き声を更新
         System.out.println("********************************");
         System.out.println("* " + pokemonList.get(2).getName() +"の技と鳴き声を変更します *");
         System.out.println("********************************");
-        changeSkill(pokemonList, Arrays.asList("メガトンパンチ","メガトンキック","ほのおのキバ"),2);
-        changeMeow(pokemonList, "ドゥワンイン",2);
+        pokemonList.get(2).setSkill(Arrays.asList("メガトンパンチ","メガトンキック","ほのおのキバ"));
+        pokemonList.get(2).setMeow("ドゥワンイン");
 
-        printMessage(pokemonList);
-
+        // 作成したポケモンを表示
+        for(Pokemon pokemon : pokemonList) {
+            System.out.println(pokemon.toString());
+        }
     }
 }
--- lecture02/CreateClass/src/Pokemon.java
+++ lecture02/CreateClass/src/Pokemon.java
@@ -3,14 +3,14 @@ import java.util.List;
 
 public class Pokemon {
     private String name;        // 名前
-    private List<String> type;  // タイプ
-    private List<String> skill; // 技
+    private List<String> types;  // タイプ
+    private List<String> skills; // 技
     private String meow;        // 鳴き声
 
-    public Pokemon(String name, List<String> type, List<String> skill, String meow) {
+    public Pokemon(String name, List<String> types, List<String> skills, String meow) {
         this.name = name;
-        this.type = type;
-        this.skill = skill;
+        this.types = types;
+        this.skills = skills;
         this.meow = meow;
     }
 
@@ -19,15 +19,15 @@ public class Pokemon {
     }
 
     public List<String> getType() {
-        return type;
+        return types;
     }
 
     public List<String> getSkill() {
-        return skill;
+        return skills;
     }
 
-    public void setSkill(List<String> skill) {
-        this.skill = skill;
+    public void setSkill(List<String> skills) {
+        this.skills = skills;
     }
 
     public String getMeow() {
@@ -36,5 +36,17 @@ public class Pokemon {
 
     public void setMeow(String meow) {
         this.meow = meow;
+    }
+
+    @Override
+    public String toString(){
+        return  """
+                作成したポケモンは以下のようになりました
+                名称：%s
+                タイプ：%s
+                技：%s
+                なきごえ：%s
+                ---------------------------------\
+                """.formatted(name, types, skills, meow);
     }
 }
