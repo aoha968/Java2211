4524ca0b5630b1e3c17efd39cbd4bd7b4031409d
aoha <aoha.kurobane168@gmail.com>
2022-11-23 13:37:23 +0900

    tryブロックの開始位置変更、Mapで受け取るように変更、コード整形(空白行の追加)

--- lecture03/List_Map/src/Main.java
+++ lecture03/List_Map/src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,15 +12,15 @@ public class Main {
                 """);
         List<String> pokemonList = new ArrayList<>();
 
-        try {
-            pokemonList.add("ヒポポタス");
-            pokemonList.add("ドクケイル");
-            pokemonList.add("ヤルキモノ");
-            pokemonList.add("グラードン");
-            pokemonList.add("ふしぎなタマゴ");
-            pokemonList.add("なぞのタマゴ");
+        pokemonList.add("ヒポポタス");
+        pokemonList.add("ドクケイル");
+        pokemonList.add("ヤルキモノ");
+        pokemonList.add("グラードン");
+        pokemonList.add("ふしぎなタマゴ");
+        pokemonList.add("なぞのタマゴ");
 
-            System.out.println("あなたの手持ちのポケモンを表示");
+        System.out.println("あなたの手持ちのポケモンを表示");
+        try {
             pokemonList.forEach(System.out::println);
 
             // 手持ちできる数を超える
@@ -34,22 +35,22 @@ public class Main {
                 +      String型のMap       +
                 ---------------------------
                 """);
-        HashMap<String, String> pokemon = new HashMap<>();
+        Map<String, String> pokemon = new HashMap<>();
+
+        pokemon.put("ピジョット", "エサを 探すとき 水面 すれすれを 滑るように 飛んで コイキングなどを わしづかみにする。");
+        pokemon.put("スターミー", "きかがくてきな ボディーから うちゅうせいぶつ ではないかと じもとでは うたがわれている。");
+        pokemon.put("イシツブテ", "まるくて もちやすいので つかんで あいてに なげて ぶつける イシツブテ がっせんが できる。");
+        pokemon.put("ユンゲラー", "あるあさのこと。 ちょうのうりょく しょうねんが ベッドから めざめると ユンゲラーに へんしん していた。");
+        pokemon.put("カイリュー", "大きな 体格で 空を 飛ぶ。地球を 約16時間で 1周してしまう。");
+
+        System.out.println("ポケモンの図鑑説明を表示");
+        System.out.println("ピジョットの図鑑説明：" + pokemon.get("ピジョット"));
+        System.out.println("スターミーの図鑑説明：" + pokemon.get("スターミー"));
+        System.out.println("イシツブテの図鑑説明：" + pokemon.get("イシツブテ"));
+        System.out.println("ユンゲラーの図鑑説明：" + pokemon.get("ユンゲラー"));
+        System.out.println("カイリューの図鑑説明：" + pokemon.get("カイリュー"));
 
         try {
-            pokemon.put("ピジョット", "エサを 探すとき 水面 すれすれを 滑るように 飛んで コイキングなどを わしづかみにする。");
-            pokemon.put("スターミー", "きかがくてきな ボディーから うちゅうせいぶつ ではないかと じもとでは うたがわれている。");
-            pokemon.put("イシツブテ", "まるくて もちやすいので つかんで あいてに なげて ぶつける イシツブテ がっせんが できる。");
-            pokemon.put("ユンゲラー", "あるあさのこと。 ちょうのうりょく しょうねんが ベッドから めざめると ユンゲラーに へんしん していた。");
-            pokemon.put("カイリュー", "大きな 体格で 空を 飛ぶ。地球を 約16時間で 1周してしまう。");
-
-            System.out.println("ポケモンの図鑑説明を表示");
-            System.out.println("ピジョットの図鑑説明：" + pokemon.get("ピジョット"));
-            System.out.println("スターミーの図鑑説明：" + pokemon.get("スターミー"));
-            System.out.println("イシツブテの図鑑説明：" + pokemon.get("イシツブテ"));
-            System.out.println("ユンゲラーの図鑑説明：" + pokemon.get("ユンゲラー"));
-            System.out.println("カイリューの図鑑説明：" + pokemon.get("カイリュー"));
-
             // 強制的にUnKnownPokemonExceptionの例外を発生させる
             System.out.println("*** 未知のポケモンの図鑑説明を表示させる ***");
             System.out.println("*** UnKnownPokemonException例外処理開始 ***");
--- lecture03/List_Map/src/UnKnownPokemonException.java
+++ lecture03/List_Map/src/UnKnownPokemonException.java
@@ -1,6 +1,7 @@
 public class UnKnownPokemonException extends Exception{
     private String code;     //独自エラーコード
     private String message;  //独自エラーメッセージ
+
     UnKnownPokemonException(String code, String message) {
         this.code = code;
         this.message = message;
